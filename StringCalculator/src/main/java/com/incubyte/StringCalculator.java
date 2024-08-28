5c80dc2798554003b5a8d18342f0badde5ea3eff
Kuldeep Panchal <kuldeepsubscription@gmail.com>
2024-08-28 08:18:31 +0530

    INCUBYTE-4 Replacing blank test with first actual test of checking add method return zero when nothing is passed

---
+++ StringCalculator/src/main/java/com/incubyte/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.incubyte;
+
+public class StringCalculator {
+
+	public int add(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- StringCalculator/src/test/java/com/incubyte/StringCalculatorTest.java
+++ StringCalculator/src/test/java/com/incubyte/StringCalculatorTest.java
@@ -1,11 +1,18 @@
 package com.incubyte;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 	
 	@Test
-	public void doNothing() {
+	public void testAdd_ReturnZero_WhenNothingIsPassedAsString() {
+		StringCalculator stringCalculator = new StringCalculator();
 		
+		int actualSum = stringCalculator.add("");
+		int expectedSum = 0;
+		
+		assertEquals(expectedSum, actualSum);
 	}	
 }
