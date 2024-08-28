328fc7c08740e89ca41e81300de869a33adb5d24
Kuldeep Panchal <kuldeepsubscription@gmail.com>
2024-08-28 08:29:21 +0530

    INCUBYTE-6 Adding the logic for add method returns 1 when 1 is passed and tested with Junit

--- StringCalculator/src/main/java/com/incubyte/StringCalculator.java
+++ StringCalculator/src/main/java/com/incubyte/StringCalculator.java
@@ -3,8 +3,14 @@ package com.incubyte;
 public class StringCalculator {
 
 	public int add(String string) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if (string.isBlank()) {
+			return 0;
+		}
+		
+		int n = Integer.valueOf(string);
+		
+		return n;
 	}
 
 }
